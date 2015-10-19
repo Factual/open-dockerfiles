048b94fb0813d3cf1289340562ba38086f2aa7b9
Nicholas Digati <nicholas@factual.com>
2015-10-19 16:17:29 -0700

    Updated s3-fuse script to get AWS credentials from EC2 instance metadata.

--- s3-backed-ftp/s3-fuse.sh
+++ s3-backed-ftp/s3-fuse.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+if [ -z $BUCKET ]; then
+  echo "You need to set BUCKET environment variable"
+  exit 1
+fi
+
+# Grab access key and secret access key from EC2 instances meta-data
+# This only works if the EC2 instance has been configured with attached IAM role
+
+instance_profile='curl http://169.254.169.254/latest/meta-data/iam/security-credentials/'
+
+AWS_ACCESS_KEY_ID="curl http://169.254.169.254/latest/meta-data/iam/security-credentials/${instance_profile} | grep AccessKeyId | cut -d':' -f2 | sed 's/[^0-9A-Z]*//g'"
+AWS_SECRET_ACCESS_KEY="curl http://169.254.169.254/latest/meta-data/iam/security-credentials/${instance_profile} | grep SecretAccessKey | cut -d':' -f2 | sed 's/[^0-9A-Za-z/+=]*//g'"
+
+# If they are still not set here, there was an error retreiving the keys from the instances meta-data
+# Or the instance was not configured with the appropriate IAM role account
 if [ -z $AWS_ACCESS_KEY_ID ]; then
   echo "You need to set AWS_ACCESS_KEY_ID environment variable"
   exit 1
@@ -9,12 +24,6 @@ if [ -z $AWS_SECRET_ACCESS_KEY ]; then
   echo "You need to set AWS_SECRET_ACCESS_KEY environment variable"
   exit 1
 fi
-
-if [ -z $BUCKET ]; then
-  echo "You need to set BUCKET environment variable"
-  exit 1
-fi
-
 
 #set the aws access credentials from environment variables
 echo $AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY > ~/.passwd-s3fs
