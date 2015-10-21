84ea6801555e20fbf68e33e8edf0358a7b9ab439
Nicholas Digati <nicholas@factual.com>
2015-10-21 12:19:52 -0700

    Updated README and files to reflect new IAM role in EC2 and new options for pasv ftp connections.

--- s3-backed-ftp/README.md
+++ s3-backed-ftp/README.md
@@ -2,19 +2,27 @@
 
 An ftp/sftp server using s3fs to mount an external s3 bucket as ftp/sftp storage.
 
-# Usage
+## Usage
 
 To run:
 
 1. First replace env.list.example file with a real env.list file with correct variables filled in.
-  * Add users to USERS env variable in the format ` user:password user2:password2 ` with a space separating each user:pass combination
-  * AWS keys and AWS bucket name should **not** be surrounded by quotes
-2. Build the docker container using:
+	- Add users to USERS env variable in the format ` user:hashedpassword user2:hashedpassword2 ` with a space separating each user:pass combination
+  	- May also use non-hashed passwords if storing passwords in plaintext is fine.
+  	- Just change line ` echo $u | chpasswd -e ` -> ` echo $u | chpasswd `
+  	- New users env variable will look like ` user:password1 user2:password2 `
+	- AWS keys are now fetched from the EC2 instance currently running the docker container
+  	- If the EC2 instance has an attached IAM role just add the roles name to s3-fuse.sh file
+  		- Add role name to option ` -o iam_role="rolenamehere" `
+  	- If there is no role account attached to EC2 instance (or not running in EC2) pass AWS access key and AWS secret access key as environment variables and uncomment lines 33 and 34 from s3-fuse.sh script
+2. Update vsftpd.conf file to add pasv_address of your ftp server
+	- Can also change the ports you want to allow passive ftp connections on by changing ` pasv_min_port ` and ` pasv_max_port `  
+3. Build the docker container using:
 
- ``` docker build --rm -t <docker/tag> path/to/dockerfile/folder ```
+	- ``` docker build --rm -t <docker/tag> path/to/dockerfile/folder ```
 
-3. Then after building the container, run using:
+4. Then after building the container, run using:
 
- ``` docker run --rm -p 21:21 -p 22:22 --name s3ftp --cap-add SYS_ADMIN --device /dev/fuse --env-file env.list -P <docker/tag> ```
-  * If env.list file is named differently change accordingly. 
+ 	- ``` docker run --rm -p 21:21 -p 22:22 -p 1024-1048 --name <name> --cap-add SYS_ADMIN --device /dev/fuse --env-file env.list -P <docker/tag> ```
+	- If env.list file is named differently change accordingly. 
 
--- s3-backed-ftp/s3-fuse.sh
+++ s3-backed-ftp/s3-fuse.sh
@@ -5,35 +5,36 @@ if [ -z $BUCKET ]; then
   exit 1
 fi
 
-# Grab access key and secret access key from EC2 instances meta-data
+# Code to grab access key and secret access key from EC2 instances meta-data
 # This only works if the EC2 instance has been configured with attached IAM role
 
-if ! curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/ | grep -q 404; then
-  instance_profile=$(curl http://169.254.169.254/latest/meta-data/iam/security-credentials/)
-
-  AWS_ACCESS_KEY_ID=$(curl http://169.254.169.254/latest/meta-data/iam/security-credentials/${instance_profile} | grep AccessKeyId | cut -d':' -f2 | sed 's/[^0-9A-Z]*//g')
-  AWS_SECRET_ACCESS_KEY=$(curl http://169.254.169.254/latest/meta-data/iam/security-credentials/${instance_profile} | grep SecretAccessKey | cut -d':' -f2 | sed 's/[^0-9A-Za-z/+=]*//g')
-else
-  "IAM Role account not linked to current EC2 instance, looking for credentials in environment variables instead..."
-fi
-
-# If they are still not set here, there was an error retreiving the keys from the instances meta-data
-# Or the instance was not configured with the appropriate IAM role account 
-# Check to make sure they are set in environment variables
-if [ -z $AWS_ACCESS_KEY_ID ]; then
-  echo "You need to set AWS_ACCESS_KEY_ID environment variable"
-  exit 1
-fi
-
-if [ -z $AWS_SECRET_ACCESS_KEY ]; then
-  echo "You need to set AWS_SECRET_ACCESS_KEY environment variable"
-  exit 1
-fi
+# if ! curl -s http://169.254.169.254/latest/meta-data/iam/security-credentials/ | grep -q 404; then
+#   instance_profile=$(curl http://169.254.169.254/latest/meta-data/iam/security-credentials/)
+# 
+#   AWS_ACCESS_KEY_ID=$(curl http://169.254.169.254/latest/meta-data/iam/security-credentials/${instance_profile} | grep AccessKeyId | cut -d':' -f2 | sed 's/[^0-9A-Z]*//g')
+#   AWS_SECRET_ACCESS_KEY=$(curl http://169.254.169.254/latest/meta-data/iam/security-credentials/${instance_profile} | grep SecretAccessKey | cut -d':' -f2 | sed 's/[^0-9A-Za-z/+=]*//g')
+# else
+#   "IAM Role account not linked to current EC2 instance, looking for credentials in environment variables instead..."
+# fi
+# 
+# # If they are still not set here, there was an error retreiving the keys from the instances meta-data
+# # Or the instance was not configured with the appropriate IAM role account 
+# # Check to make sure they are set in environment variables
+# if [ -z $AWS_ACCESS_KEY_ID ]; then
+#   echo "You need to set AWS_ACCESS_KEY_ID environment variable"
+#   exit 1
+# fi
+# 
+# if [ -z $AWS_SECRET_ACCESS_KEY ]; then
+#   echo "You need to set AWS_SECRET_ACCESS_KEY environment variable"
+#   exit 1
+# fi
 
 #set the aws access credentials from environment variables
-echo $AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY > ~/.passwd-s3fs
-chmod 600 ~/.passwd-s3fs
+# echo $AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY > ~/.passwd-s3fs
+# chmod 600 ~/.passwd-s3fs
 
-#start s3 fuse
-
-/usr/local/bin/s3fs $BUCKET /home/aws/s3bucket -o allow_other -o mp_umask="0022" #-d -d -f -o f2 -o curldbg
+# start s3 fuse
+# Code above is not needed if the IAM role is attaced to EC2 instance 
+# s3fs provides the iam_role option to grab those credentials automatically
+/usr/local/bin/s3fs $BUCKET /home/aws/s3bucket -o allow_other -o mp_umask="0022" -o iam_role="" #-d -d -f -o f2 -o curldbg
