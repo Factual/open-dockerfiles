fa44fbf9103134e3ddc47739a477823946328fb8
Nicholas Digati <nicholas@factual.com>
2015-12-01 13:07:30 -0800

    Fixed issue when files uploaded directly through S3 console would have incorrect permissions.

--- s3-backed-ftp/add_users_in_container.sh
+++ s3-backed-ftp/add_users_in_container.sh
@@ -5,6 +5,10 @@
 FTP_DIRECTORY="/home/aws/s3bucket/ftp-users"
 CONFIG_FILE="env.list" # May need to modify config file name to reflect future changes in env file location/name
 SLEEP_DURATION=60
+# Change theses next two variables to set different permissions for files/directories
+# These were default from vsftpd so change accordingly if necessary
+FILE_PERMISSIONS=644
+DIRECTORY_PERMISSIONS=755
 
 add_users() {
   aws s3 cp s3://$CONFIG_BUCKET/$CONFIG_FILE ~/$CONFIG_FILE
@@ -17,6 +21,21 @@ add_users() {
     # In cases where password changes in env file
     if getent passwd "$username" >/dev/null 2>&1; then
       echo $u | chpasswd -e
+
+      # Fix for issue when pulling files that were uploaded directly to S3 (through aws web console)
+      # Permissions when uploaded directly through S3 Web client were set as:
+      # 000 root:root
+      # This would not allow ftp users to read the files
+      
+      # Search for files and directories not owned correctly
+      find "$FTP_DIRECTORY"/"$username"/files/* \( \! -user "$username" \! -group "$username" \) -print0 | xargs -0 chown "$username:$username"
+
+      # Search for files with incorrect permissions
+      find "$FTP_DIRECTORY"/"$username"/files/* -type f \! -perm "$FILE_PERMISSIONS" -print0 | xargs -0 chmod "$FILE_PERMISSIONS"
+
+      # Search for directories with incorrect permissions
+      find "$FTP_DIRECTORY"/"$username"/files/* -type d \! -perm "$DIRECTORY_PERMISSIONS" -print0 | xargs -0 chmod "$DIRECTORY_PERMISSIONS"
+
     fi
 
     # If user account doesn't exist create it 
