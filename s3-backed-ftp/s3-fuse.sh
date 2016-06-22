4d59bafb947e77901ddb85606b496b9301adce3a
Nicholas Digati <nicholas@factual.com>
2016-06-22 16:41:23 -0700

    Added option to s3fs to expire stat cache to hopefully fix memory leak on EC2 instance.

--- s3-backed-ftp/s3-fuse.sh
+++ s3-backed-ftp/s3-fuse.sh
@@ -51,5 +51,5 @@ fi
 # start s3 fuse
 # Code above is not needed if the IAM role is attaced to EC2 instance
 # s3fs provides the iam_role option to grab those credentials automatically
-/usr/local/bin/s3fs $FTP_BUCKET /home/aws/s3bucket -o allow_other -o mp_umask="0022" -o iam_role="$IAM_ROLE" #-d -d -f -o f2 -o curldbg
+/usr/local/bin/s3fs $FTP_BUCKET /home/aws/s3bucket -o allow_other -o mp_umask="0022" -o iam_role="$IAM_ROLE" -o stat_cache_expire=600 #-d -d -f -o f2 -o curldbg
 /usr/local/users.sh
